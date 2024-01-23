8ed913343b26aa779ba9e5982467095a77af3482
kamronbek-olimov-1500196 <127713658+kamronbek-olimov-1500196@users.noreply.github.com>
2024-01-23 22:52:55 +0500

    Update task_1.py

--- src/cpp/task_1.cpp
+++ src/cpp/task_1.cpp
@@ -1,9 +1,17 @@
-/*
- * Author:
- * Date:
- * Name:
- */
-
-class Problem1 {
-public:
-};
+def findWays(nums, target):
+    memo = {}
+    def dfs(index, current_sum):
+        if index == len(nums):
+            if current_sum == target:
+                return 1
+            return 0
+        if (index, current_sum) in memo:
+            return memo[(index, current_sum)]
+        ways = dfs(index + 1, current_sum + nums[index]) + dfs(index + 1, current_sum - nums[index])
+        memo[(index, current_sum)] = ways
+        return ways
+    return dfs(0, 0)
+nums = list(map(int, input("nums = ").split()))
+target = int(input("target = "))
+result = findWays(nums, target)
+print(result)
